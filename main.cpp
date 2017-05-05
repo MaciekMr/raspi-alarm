5970287208267533b7dfad7f2a7535ac2817f35f
MaciekMr <maciej_m@mailplus.pl>
2017-05-05 12:09:09 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,34 +1,11 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include <bcm2835.h>
-#include "bcm_lib.h"
-#include "pin.h"
-#include "gpio.h"
+#include "mainwindow.h"
+#include <QApplication>
 
-
-using namespace std;
-
-int main (int n, char ** par)
+int main(int argc, char *argv[])
 {
-	/*
-	if(!bcm2835_init())
-		return 1;
-	cout << "initiated \n";	
-	*/
-	//CPin pin();
-	
-	//bcm2835 _bcm2835;
-	
-	//printf("before CGPIO %d \n", gpio.addr_p);
-	//printf("before CGPIO %d \n", _bcm2835.addr_p);
-	
-	CGPIO gp;
-	
-	//printf("after CGPIO %d \n", gpio.addr_p);
-	//printf("before CGPIO %d \n", _bcm2835.addr_p);
-	
-	
-	
-	return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
